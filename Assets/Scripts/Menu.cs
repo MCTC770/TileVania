3b8edab98e8f0ac99986b26fc42f17580b152e0e
DESKTOP-HCMRCN5\mc-tc <tomcolin.horn@gmail.com>
2018-09-23 00:15:36 +0200

    Added start and end screen

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour {
+	
+	public void LoadFirstLevel()
+	{
+		int currentScene = SceneManager.GetActiveScene().buildIndex + 1;
+		SceneManager.LoadScene(currentScene, LoadSceneMode.Single);
+	}
+
+	public void LoadMainMenu()
+	{
+		SceneManager.LoadScene("Main Menu", LoadSceneMode.Single);
+	}
+}
