1646197a0e3aaa89597533e1a9be4c4f22beb86d
DESKTOP-HCMRCN5\mc-tc <tomcolin.horn@gmail.com>
2018-09-24 23:48:51 +0200

    Added coins as pickups

---
+++ Assets/Scripts/CoinPickup.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinPickup : MonoBehaviour {
+
+	private void OnTriggerEnter2D(Collider2D collider)
+	{
+		if (collider.gameObject.name == "Player")
+		{
+			Destroy(gameObject);
+		}
+	}
+}
