7b0839734f4d3e4d17595b7056dd1df7c3462661
DESKTOP-HCMRCN5\mc-tc <tomcolin.horn@gmail.com>
2018-09-29 22:24:33 +0200

    Persist coin pick-ups

---
+++ Assets/Scripts/ScenePersist.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScenePersist : MonoBehaviour {
+
+	int startingScene;
+
+	private void Awake()
+	{
+		int numberOfScenePersists = FindObjectsOfType<ScenePersist>().Length;
+
+		if (numberOfScenePersists > 1)
+		{
+			Destroy(gameObject);
+		}
+		else
+		{
+			DontDestroyOnLoad(gameObject);
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		startingScene = SceneManager.GetActiveScene().buildIndex;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (startingScene != SceneManager.GetActiveScene().buildIndex)
+		{
+			Destroy(gameObject);
+		}
+	}
+}
