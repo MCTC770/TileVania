b60752ce345311dbd3464bd0b9916f4f615ff335
DESKTOP-HCMRCN5\mc-tc <tomcolin.horn@gmail.com>
2018-10-01 16:34:22 +0200

    Progress made with star pickup UI

--- Assets/Scripts/StarCounter.cs
+++ Assets/Scripts/StarCounter.cs
@@ -8,16 +8,39 @@ public class StarCounter : MonoBehaviour {
 	public int starsCollected = 0;
 	public int starsTotal = 0;
 	[SerializeField] Text starText;
+	[SerializeField] Image firstStarUI;
+	GameObject firstStar;
+	[SerializeField] Image secondStarUI;
+	GameObject secondStar;
+	[SerializeField] Image thirdStarUI;
+	GameObject thirdStar;
+	[SerializeField] Image fullStar;
 	GameSession currentGameSession;
 
 	void Start ()
 	{
+		firstStar = GameObject.Find("Star1");
+		secondStar = GameObject.Find("Star2");
+		thirdStar = GameObject.Find("Star3");
 		starsTotal = FindObjectsOfType<StarPickup>().Length;
 		currentGameSession = FindObjectOfType<GameSession>();
 	}
 
 	private void Update()
 	{
+		print(firstStar);
+		if (firstStar == null)
+		{
+			firstStarUI.GetComponent<Image>().sprite = fullStar.GetComponent<Image>().sprite;
+		}
+		if (secondStar == null)
+		{
+			secondStarUI.GetComponent<Image>().sprite = fullStar.GetComponent<Image>().sprite;
+		}
+		if (thirdStar == null)
+		{
+			thirdStarUI.GetComponent<Image>().sprite = fullStar.GetComponent<Image>().sprite;
+		}
 		starsCollected = currentGameSession.starsCollected;
 		starText.text = starsCollected.ToString() + "/" + "3";
 	}
