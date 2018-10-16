6f135f8eee12b3c9a469b31bb84c2d87d69de8a2
DESKTOP-HCMRCN5\mc-tc <tomcolin.horn@gmail.com>
2018-10-16 23:55:08 +0200

    started with level 2 script

--- Assets/Scripts/GameSession.cs
+++ Assets/Scripts/GameSession.cs
@@ -46,7 +46,6 @@ public class GameSession : MonoBehaviour {
 
 	private void Update()
 	{
-		print(checkpoints);
 		UpdateUITexts();
 	}
 
---
+++ Assets/Scripts/Level2Tutorial.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level2Tutorial : MonoBehaviour {
+
+	[SerializeField] GameObject collectPlayerTutorial;
+	[SerializeField] GameObject doubleJumpTutorial;
+	[SerializeField] GameObject noPlayerTutorial;
+	[SerializeField] GameObject collectMorePlayerTutorial;
+	[SerializeField] GameObject multiJumpTutorial;
+	[SerializeField] BoxCollider2D runTutorialCollider;
+
+	bool runTutorialTriggered = false;
+
+	/*void Start () {
+		walkTutorial.SetActive(true);
+		jumpTutorial.SetActive(true);
+	}
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		if (collision.gameObject.layer == 11 && runTutorialTriggered == true && runTutorialCollider == null)
+		{
+			runTutorial.SetActive(false);
+			longJumpTutorial.SetActive(false);
+		}
+		else if (collision.gameObject.layer == 11 && runTutorialTriggered == false)
+		{
+			Destroy(runTutorialCollider);
+			walkTutorial.SetActive(false);
+			jumpTutorial.SetActive(false);
+			runTutorial.SetActive(true);
+			longJumpTutorial.SetActive(true);
+
+			runTutorialTriggered = true;
+		}
+	}*/
+}
