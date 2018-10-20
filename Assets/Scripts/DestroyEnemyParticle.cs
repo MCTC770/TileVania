a754372de289a6dc343095811ae5ef2f4c7a25cb
DESKTOP-HCMRCN5\mc-tc <tomcolin.horn@gmail.com>
2018-10-21 00:35:24 +0200

    Started work on level 3 and made enemy particles selfdestruct after they played

---
+++ Assets/Scripts/DestroyEnemyParticle.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyEnemyParticle : MonoBehaviour {
+
+	float particleLifeTime;
+
+	// Use this for initialization
+	void Start () {
+		particleLifeTime = GetComponent<ParticleSystem>().main.duration;
+		StartCoroutine(SelfDestruct(particleLifeTime));
+	}
+	
+	IEnumerator SelfDestruct(float particleLifeTime)
+	{
+		yield return new WaitForSeconds(particleLifeTime);
+		Destroy(gameObject);
+	}
+}
